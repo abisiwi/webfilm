94c196416e22b7214bbfeedecc0d6c744527ca3c
abisiwi <abisiwi.abisiwiroiswicaksono@gmail.com>
2022-09-21 00:39:54 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+import NavBar from './components/NavBar';
+import Intro from './components/Intro';
+import './style/landingPage.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="myBg">
+        <NavBar />
+        <Intro />
+      </div>
     </div>
   );
 }
---
+++ src/components/Intro.js
@@ -0,0 +1,20 @@
+import { Navbar, Container, Row, Col } from "react-bootstrap"
+
+const Intro = () => {
+    return (
+        <>
+            <div className="intro">
+                <Container className="text-white" >
+                    <Row>
+                        <Col>
+                            <div className="title1">WELCOME TO BEJI MARON</div>
+                            <div className="title2">Tempat asik untuk berwisata bersama keluarga</div>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        </>
+    )
+}
+
+export default Intro
